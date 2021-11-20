851ebf5eb01317df7e4787b370643ccb42592cc0
A A Karim <adil@lix-it.com>
2021-11-20 22:31:32 +0000

    add last login attribute to User

--- src/Listeners/Login.php
+++ src/Listeners/Login.php
@@ -8,7 +8,9 @@ class Login
     public function handle(LoginEvent $login)
     {
         if (config("services.mixpanel.enable-default-tracking")) {
-            event(new Mixpanel($login->user, ['User Logged In' => []]));
+            event(new Mixpanel($login->user, ['User Logged In' => []], 0, [
+                'Last Logged In' => now()->format('Y-m-d\Th:i:s'),
+            ]));
         }
     }
 }
