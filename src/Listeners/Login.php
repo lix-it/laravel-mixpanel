8024edab11447898210b182f6e8c65f3d9e994df
A A Karim <adil@aakarim.com>
2025-03-31 20:19:49 +0100

    add properties

--- src/Listeners/Login.php
+++ src/Listeners/Login.php
@@ -8,9 +8,28 @@ class Login
     public function handle(LoginEvent $login)
     {
         if (config("services.mixpanel.enable-default-tracking")) {
-            event(new Mixpanel($login->user, ['User Logged In' => []], 0, [
-                'Last Logged In' => now()->format('Y-m-d\Th:i:s'),
-            ]));
+            // Check if this is an SSO registration
+            $isSSO = request()->route() && strpos(request()->route()->uri(), 'auth/google') !== false;
+            if ($isSSO) {
+                // This is an SSO login (Google)
+                event(new Mixpanel($login->user, [
+                    'User Logged In' => [
+                        'authentication_method' => 'SSO',
+                        'sso_provider' => 'Google'
+                    ]
+                ], 0, [
+                    'Last Logged In' => now()->format('Y-m-d\Th:i:s'),
+                ]));
+            } else {
+                // This is a standard login
+                event(new Mixpanel($login->user, [
+                    'User Logged In' => [
+                        'authentication_method' => 'standard'
+                    ]
+                ], 0, [
+                    'Last Logged In' => now()->format('Y-m-d\Th:i:s'),
+                ]));
+            }
         }
     }
 }
